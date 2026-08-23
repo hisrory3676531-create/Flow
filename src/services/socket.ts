50dae5fbecef8a1f61395cedf7867a0108e1f870
hisrory3676531-create <hisrory3676531-create@gmail.com>
2026-08-23 20:40:44 +0700

    Fix socket backend url

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,12 +1,7 @@
 import { io } from 'socket.io-client';
 
-const rawUrl = (import.meta as any).env?.VITE_SERVER_URL || 'http://localhost:3001';
-// Удаляем возможные дубликаты слешей в конце
-const SERVER_URL = rawUrl.replace(/\/+$/, '');
+const BACKEND_URL = 'https://flow-5ty0.onrender.com';
 
-export const socket = io(SERVER_URL, {
-  transports: ['websocket', 'polling'],
-  reconnection: true,
-  reconnectionAttempts: 10,
-  reconnectionDelay: 1000
+export const socket = io(BACKEND_URL, {
+  transports: ['websocket', 'polling']
 });
